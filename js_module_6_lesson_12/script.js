002e1552847ecca09911541094a5717ff230c5d5
Phanes666 <mephistophelesofua@gmail.com>
2023-07-30 18:50:48 +0300

    ///////

--- js_module_6_lesson_12/script.js
+++ js_module_6_lesson_12/script.js
@@ -1,24 +1,43 @@
+// const elements = {
+//     clickMe: document.querySelector('.js-click'),
+//     box: document.querySelector('.js-box')
+// }
+
+// elements.clickMe.addEventListener('click', handlerClick)
+
+// let step = 0;
+// // function handlerClick() {
+// //     step += 50;
+// //     elements.box.style.marginTop = `${step}px`;
+// //     console.log(elements.box.style.marginTop);
+// //     elements.box.style.marginLeft = `${step}px`;
+// //     console.log(elements.box.style.marginLeft);
+// //     // console.log(evt);
+// // }
+
+// function handlerClick(evt) {
+//     step += 50;
+//     evt.currentTarget.style.marginTop = `${step}px`;
+//     console.log(elements.box.style.marginTop);
+//     evt.currentTarget.style.marginLeft = `${step}px`;
+//     console.log(elements.box.style.marginLeft);
+//     // console.log(evt);
+// }
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 const elements = {
     clickMe: document.querySelector('.js-click'),
-    box: document.querySelector('.js-box')
+    box: document.querySelector('.js-box'),
+    query: document.querySelector('.js-query')
 }
 
-elements.clickMe.addEventListener('click', handlerClick)
+elements.query.addEventListener('input', handlerSearch);
+elements.query.addEventListener('change', handlerSearch);
 
-let step = 0;
-function handlerClick(evt) {
-    step += 50;
-    elements.box.style.marginTop = `${step}px`;
-    console.log(elements.box.style.marginTop);
-    elements.box.style.marginLeft = `${step}px`;
-console.log(evt);
-console.log(elements.box.style.marginLeft);
+function handlerSearch(evt) {
+    console.dir(evt.currentTarget.value);
 }
-
-
-
-
-
 
 
 
